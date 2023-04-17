5a745fd98e1c82ad80c7a0ee4ac68993589360e3
Tagline <tagline@Sairam-2.local>
2023-04-17 16:13:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       TesCases Demo Using Jest Framework
+      </header>
+    </div>
+  );
+}
+
+export default App;
